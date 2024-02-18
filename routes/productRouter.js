a55f12f6bb249f18406d11850af2d547cfa64beb
Your Name <you@example.com>
2024-02-18 01:57:25 +0100

    second

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -3,24 +3,32 @@ const router = require('express').Router();
 
 // get all products
 
-router.get('/', async (req,res)=>{
-    
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).send(products);
+  } catch (err) {
+    res.status(500).send({ message: 'something went wrong', err });
+  }
+});
+
+// get a single product
+
+router.get('/:id', async(req,res) =>{
     try{
-        const products = await Product.find()
-        res.status(200).send(products)
+       const {id} = req.params
+       const product = await Product.findById(id)
+      
+
+       if(!product){
+         res.status(404).send({message:"no product found"})
+       }else{
+        res.status(200).send(product)
+       }
     }catch(err){
-       res.status(500).send({message:"something went wrong",err})
+      res.status(500).send({message:"something went wrong",err})
     }
-
- 
-    
 })
-
-
-
-
-
-
 // post a product
 router.post('/', async (req, res) => {
   const newProduct = new Product({
